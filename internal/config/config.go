8cbfd17dd780d24d3d06fe1afa2c75833ae1a6ab
Дмитрий Родин <d.rodin@grcc.tech>
2023-05-08 19:23:24 +0300

    iter5 done

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 type Config struct {
@@ -10,9 +11,21 @@ type Config struct {
 }
 
 func NewConfig() *Config {
+	envAddr := os.Getenv("SERVER_ADDRESS")
+	envServerHost := os.Getenv("BASE_URL")
 	addr := flag.String("a", ":8080", "")
 	serverHost := flag.String("b", "http://localhost:8080", "server adress")
+
 	flag.Parse()
+
+	if envAddr != "" {
+		addr = &envAddr
+	}
+
+	if envServerHost != "" {
+		serverHost = &envServerHost
+	}
+
 	return &Config{
 		Addr:       *addr,
 		ServerHost: *serverHost,
