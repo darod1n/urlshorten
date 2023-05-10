3e46e66cd0ecd6a066067b3bd9f5b87ea9dc3d7a
Дмитрий Родин <d.rodin@grcc.tech>
2023-05-10 12:31:58 +0300

    update go version; update GenerateShortURL

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -2,12 +2,10 @@ package helpers
 
 import (
 	"math/rand"
-	"time"
 )
 
 func GenerateShortURL(lenToken int) string {
 	var shortURL []byte
-	rand.Seed(time.Now().UnixNano())
 	alphabet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	for i := 0; i < lenToken; i++ {
 		r := rand.Intn(len(alphabet))
