ba7997560261ea4109fe158bdf7441c236594e00
Дмитрий Родин <d.rodin@grcc.tech>
2023-05-10 12:11:41 +0300

    move GenerateShortURL to util; update storage

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,46 +4,24 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/darod1n/urlshorten/internal/storage"
-
-	"github.com/gorilla/mux"
+	"github.com/darod1n/urlshortener/internal/storage"
 )
 
-type Storage interface {
-	GenerateToken(lenToken int) string
-	GetStrFromFile(path string) string
-	SaveToFile(path string, str string)
-	IsExist(token string) bool
+func APIShortURL(serverHost string, db storage.Storage, res http.ResponseWriter, req *http.Request) {
+	if body, err := io.ReadAll(req.Body); err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+	} else {
+		shortURL := db.GenerateShortURL(6)
+		db.AddToStorage(string(body), shortURL)
+		res.WriteHeader(http.StatusCreated)
+		res.Write([]byte(serverHost + req.RequestURI + shortURL))
+	}
+
 }
 
-func GenerateToken(lenToken int) string {
-	return storage.GenerateToken(lenToken)
-}
-
-func GetStrFromFile(path string) string {
-	return storage.GetStrFromFile(path)
-}
-
-func SaveToFile(path string, str string) {
-	storage.SaveToFile(path, str)
-}
-
-func IsExist(token string) bool {
-	return storage.IsExist(token)
-}
-
-func APIShortURL(serverHost string, db Storage, res http.ResponseWriter, req *http.Request) {
-	body, _ := io.ReadAll(req.Body)
-	token := db.GenerateToken(6)
-	db.SaveToFile(token, string(body))
-	res.WriteHeader(http.StatusCreated)
-	res.Write([]byte(serverHost + req.RequestURI + token))
-}
-
-func APIGetBigURL(db Storage, res http.ResponseWriter, req *http.Request) {
-	token := mux.Vars(req)["token"]
-	if db.IsExist(token) {
-		bigURL := db.GetStrFromFile(token)
+func APIGetBigURL(db storage.Storage, res http.ResponseWriter, req *http.Request) {
+	shortURL := req.RequestURI[1:]
+	if bigURL, ok := db.GetURL(shortURL); ok {
 		res.Header().Set("Location", bigURL)
 		res.WriteHeader(http.StatusTemporaryRedirect)
 	} else {
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,34 +1,18 @@
 package storage
 
-import (
-	"math/rand"
-	"time"
-)
-
-type Storage struct {
+type DB struct {
 	urls map[string]string
 }
 
-func (s *Storage) GenerateShortURL(lenToken int) string {
-	var shortURL []byte
-	rand.Seed(time.Now().UnixNano())
-	alphabet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	for i := 0; i < lenToken; i++ {
-		r := rand.Intn(len(alphabet))
-		shortURL = append(shortURL, alphabet[r])
-	}
-	return string(shortURL)
+func (db *DB) AddURL(url string, shortURL string) {
+	db.urls[shortURL] = url
 }
 
-func (s *Storage) AddToStorage(url string, shortURL string) {
-	s.urls[shortURL] = url
-}
-
-func (s *Storage) GetURL(shortURL string) (string, bool) {
-	bigURL, ok := s.urls[shortURL]
+func (db *DB) GetURL(shortURL string) (string, bool) {
+	bigURL, ok := db.urls[shortURL]
 	return bigURL, ok
 }
 
-func NewStorage() *Storage {
-	return &Storage{urls: map[string]string{}}
+func NewDB() *DB {
+	return &DB{urls: map[string]string{}}
 }
