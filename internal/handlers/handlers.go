f83784b372733937c1488c56f0e4ad77f57d877f
Дмитрий Родин <d.rodin@grcc.tech>
2023-05-10 12:13:09 +0300

    update handlers logic

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,22 +4,40 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/darod1n/urlshortener/internal/storage"
+	"github.com/darod1n/urlshorten/internal/storage"
+	"github.com/darod1n/urlshorten/internal/util"
 )
 
-func APIShortURL(serverHost string, db storage.Storage, res http.ResponseWriter, req *http.Request) {
+type DB struct {
+	storage.DB
+}
+
+type Storage interface {
+	AddURL(url string, shortURL string)
+	GetURL(shortURL string) (string, bool)
+}
+
+func (db *DB) AddURL(url string, shortURL string) {
+	db.AddURL(url, shortURL)
+}
+
+func (db *DB) GetURL(shortURL string) (string, bool) {
+	return db.GetURL(shortURL)
+}
+
+func APIShortURL(serverHost string, db Storage, res http.ResponseWriter, req *http.Request) {
 	if body, err := io.ReadAll(req.Body); err != nil {
 		res.WriteHeader(http.StatusBadRequest)
 	} else {
-		shortURL := db.GenerateShortURL(6)
-		db.AddToStorage(string(body), shortURL)
+		shortURL := util.GenerateShortURL(6)
+		db.AddURL(string(body), shortURL)
 		res.WriteHeader(http.StatusCreated)
 		res.Write([]byte(serverHost + req.RequestURI + shortURL))
 	}
 
 }
 
-func APIGetBigURL(db storage.Storage, res http.ResponseWriter, req *http.Request) {
+func APIGetBigURL(db Storage, res http.ResponseWriter, req *http.Request) {
 	shortURL := req.RequestURI[1:]
 	if bigURL, ok := db.GetURL(shortURL); ok {
 		res.Header().Set("Location", bigURL)
