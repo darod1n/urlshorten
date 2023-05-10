75894b4284934c4bc4133980d2bf0dc45328827a
Дмитрий Родин <d.rodin@grcc.tech>
2023-05-10 12:16:37 +0300

    add chi router

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,55 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/darod1n/urlshorten/internal/config"
 	"github.com/darod1n/urlshorten/internal/handlers"
 	"github.com/darod1n/urlshorten/internal/storage"
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi/v5"
 )
-
-type Storage interface {
-	GenerateToken(lenToken int) string
-	GetStrFromFile(path string) string
-	SaveToFile(path string, str string)
-	IsExist(token string) bool
-}
-
-type DBStorage struct{}
-
-func (db *DBStorage) GenerateToken(lenToken int) string {
-	return storage.GenerateToken(lenToken)
-}
-
-func (db *DBStorage) GetStrFromFile(path string) string {
-	return storage.GetStrFromFile(path)
-}
-
-func (db *DBStorage) SaveToFile(path string, str string) {
-	storage.SaveToFile(path, str)
-}
-
-func (db *DBStorage) IsExist(token string) bool {
-	return storage.IsExist(token)
-}
 
 func main() {
 	serverConfig := config.NewConfig()
 
-	router := mux.NewRouter()
+	router := chi.NewRouter()
 
-	db := &DBStorage{}
-	router.HandleFunc("/{token:[a-zA-z0-9]+}", func(w http.ResponseWriter, r *http.Request) {
-		t := mux.Vars(r)["token"]
-		fmt.Println(t)
+	db := storage.NewDB()
+	router.Get("/{shortURL:[a-zA-Z0-9]+}", func(w http.ResponseWriter, r *http.Request) {
 		handlers.APIGetBigURL(db, w, r)
+	})
 
-	}).Methods("GET")
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router.Post("/", func(w http.ResponseWriter, r *http.Request) {
 		handlers.APIShortURL(serverConfig.ServerHost, db, w, r)
-	}).Methods("POST")
+	})
 
 	http.Handle("/", router)
 
