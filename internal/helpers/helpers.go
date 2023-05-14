91927a2aec2705ea512a33a619b4cc8348d00cc7
Дмитрий Родин <d.rodin@grcc.tech>
2023-05-14 21:02:20 +0300

    alphabet - const

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -4,9 +4,12 @@ import (
 	"math/rand"
 )
 
+const (
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
+
 func GenerateShortURL(lenToken int) string {
 	shortURL := make([]byte, 0, lenToken)
-	alphabet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	for i := 0; i < lenToken; i++ {
 		r := rand.Intn(len(alphabet))
 		shortURL = append(shortURL, alphabet[r])
