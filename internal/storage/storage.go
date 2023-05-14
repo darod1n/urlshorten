1e09f54e7218f8e8b811fbbfa1127e9414089569
Дмитрий Родин <d.rodin@grcc.tech>
2023-05-14 20:58:05 +0300

    add mutex

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,18 +4,18 @@ import "sync"
 
 type DB struct {
 	urls map[string]string
-	mu   sync.Mutex
+	sync.Mutex
 }
 
 func (db *DB) AddURL(url string, shortURL string) {
-	db.mu.Lock()
-	defer db.mu.Unlock()
+	db.Lock()
+	defer db.Unlock()
 	db.urls[shortURL] = url
 }
 
 func (db *DB) GetURL(shortURL string) (string, bool) {
-	db.mu.Lock()
-	defer db.mu.Unlock()
+	db.Lock()
+	defer db.Unlock()
 	bigURL, ok := db.urls[shortURL]
 	return bigURL, ok
 }
