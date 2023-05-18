7307a47d91cd436ef5940b3f0766e41e9994dc97
Дмитрий Родин <d.rodin@grcc.tech>
2023-05-18 23:14:42 +0300

    add func WithLoggin

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,27 @@
 package logger
+
+import (
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+var sugar zap.SugaredLogger
+
+type Storage interface{}
+
+func WithLoggin(s Storage, w http.ResponseWriter, res *http.Request) {
+	start := time.Now()
+
+	uri := res.RequestURI
+	method := res.Method
+
+	duration := time.Since(start)
+
+	sugar.Infow(
+		"uri", uri,
+		"method", method,
+		"duration", duration,
+	)
+}
