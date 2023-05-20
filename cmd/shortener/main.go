4fc34b2e29be698485814d1f27279280655dba9a
Дмитрий Родин <d.rodin@grcc.tech>
2023-05-20 20:28:26 +0300

    iter6 done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,29 +6,37 @@ import (
 
 	"github.com/darod1n/urlshorten/internal/config"
 	"github.com/darod1n/urlshorten/internal/handlers"
+	"github.com/darod1n/urlshorten/internal/logger"
 	"github.com/darod1n/urlshorten/internal/storage"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+
 	serverConfig := config.NewConfig()
+	db := storage.NewDB()
+	if err := logger.InitializeLoger(); err != nil {
+		log.Fatal(err)
+	}
 
 	router := chi.NewRouter()
-
-	db := storage.NewDB()
-	router.Get("/{shortURL}", func(w http.ResponseWriter, r *http.Request) {
+	router.With(logger.WithLoggin).Get("/{shortURL}", func(w http.ResponseWriter, r *http.Request) {
 		shortURL := chi.URLParam(r, "shortURL")
 		handlers.GetBigURL(shortURL, db, w, r)
 	})
-
-	router.Post("/", func(w http.ResponseWriter, r *http.Request) {
+	router.With(logger.WithLoggin).Post("/", func(w http.ResponseWriter, r *http.Request) {
 		handlers.ShortURL(serverConfig.ServerHost, db, w, r)
 	})
 
 	http.Handle("/", router)
 
-	err := http.ListenAndServe(serverConfig.Addr, nil)
-	if err != nil {
-		log.Fatal(err)
+	logger.Sugar.Infow(
+		"Starting server",
+		"Addr", serverConfig.Addr,
+	)
+
+	errServer := http.ListenAndServe(serverConfig.Addr, nil)
+	if errServer != nil {
+		log.Fatal(errServer)
 	}
 }
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -7,21 +7,80 @@ import (
 	"go.uber.org/zap"
 )
 
-var sugar zap.SugaredLogger
+type ResponseWriter interface {
+	Header() http.Header
+	Write([]byte) (int, error)
+	WriteHeader(statusCode int)
+}
 
-type Storage interface{}
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
 
-func WithLoggin(s Storage, w http.ResponseWriter, res *http.Request) {
-	start := time.Now()
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
 
-	uri := res.RequestURI
-	method := res.Method
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
 
-	duration := time.Since(start)
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
 
-	sugar.Infow(
-		"uri", uri,
-		"method", method,
-		"duration", duration,
-	)
+var Sugar zap.SugaredLogger
+
+func InitializeLoger() error {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		return err
+	}
+	defer logger.Sync()
+
+	// делаем регистратор SugaredLogger
+	Sugar = *logger.Sugar()
+	return nil
+}
+
+func WithLoggin(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, res *http.Request) {
+		start := time.Now()
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
+			responseData:   responseData,
+		}
+
+		uri := res.RequestURI
+		method := res.Method
+
+		h.ServeHTTP(&lw, res)
+
+		duration := time.Since(start)
+
+		Sugar.Infoln(
+			"uri", uri,
+			"method", method,
+			"status", responseData.status, // получаем перехваченный код статуса ответа
+			"duration", duration,
+			"size", responseData.size, // получаем перехваченный размер ответа
+		)
+	})
+
 }
