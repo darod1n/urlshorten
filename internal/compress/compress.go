33a2af15da64df1ba1d7d66770b349754b8f8990
Дмитрий Родин <d.rodin@grcc.tech>
2023-05-23 19:41:32 +0300

    create comress func WithCompress

---
+++ internal/compress/compress.go
@@ -0,0 +1,44 @@
+package compress
+
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type gzipWriter struct {
+	http.ResponseWriter
+	Writer io.Writer
+}
+
+func (w gzipWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func WithCompress(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		contentType := r.Header.Get("Content-Type")
+
+		if !strings.Contains(contentType, "application/json") && !strings.Contains(contentType, "text/html") {
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		if r.Header.Get("") != "gzip" {
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+		if err != nil {
+
+			return
+		}
+
+		defer gz.Close()
+
+		w.Header().Set("Content-Encoding", "gzip")
+		h.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+	})
+}
