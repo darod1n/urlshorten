eb229c3fccd2e89fcec7d42f5468c9ad69868330
Дмитрий Родин <d.rodin@grcc.tech>
2023-05-30 13:38:39 +0300

    add flag path

--- internal/storage/flags.go
+++ internal/storage/flags.go
@@ -1,9 +1,17 @@
 package storage
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var path string
 
 func init() {
-	flag.StringVar(&path, "f", "tmp/test.json", "File path")
+
+	flag.StringVar(&path, "f", "tmp/short-url-db.json", "File path")
+
+	if fp := os.Getenv("FILE_STORAGE_PATH"); fp != "" {
+		path = fp
+	}
 }
