9c249ca29a167bd32cd726c0d4842c57945577b9
Дмитрий Родин <d.rodin@grcc.tech>
2023-06-13 21:08:48 +0300

    feat: create models

---
+++ internal/models/models.go
@@ -0,0 +1,11 @@
+package models
+
+type BatchRequest struct {
+	CorrelationID string `json:"correlation_id"`
+	OriginURL     string `json:"original_url"`
+}
+
+type BatchResponse struct {
+	CorrelationID string `json:"correlation_id"`
+	ShortURL      string `json:"short_url"`
+}
