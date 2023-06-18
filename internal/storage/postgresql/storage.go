51de97f3be20d156382c1c21f21ef8625c47abe1
Дмитрий Родин <d.rodin@grcc.tech>
2023-06-18 20:19:31 +0300

    refactor: refactor create database query

--- internal/storage/postgresql/storage.go
+++ internal/storage/postgresql/storage.go
@@ -70,59 +70,7 @@ func (db *DB) Batch(ctx context.Context, host string, batch []models.BatchReques
 }
 
 func createDB(ctx context.Context, db *sql.DB) error {
-	_, err := db.ExecContext(ctx, `
-	create table if not exists urls(
-		short_url text primary key,
-		original_url text unique
-	);
-	
-	Create or replace function random_string() returns text as
-	$$
-	declare
-	  chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
-	  result text := '';
-	  i integer := 0;
-	  length integer := 6;
-	begin
-	  if length < 0 then
-		raise exception 'Given length cannot be less than 0';
-	  end if;
-	  for i in 1..length loop
-		result := result || chars[1+random()*(array_length(chars, 1)-1)];
-	  end loop;
-	  return result;
-	end;
-	$$ language plpgsql;
-	
-	CREATE OR REPLACE FUNCTION unique_short_id()
-	RETURNS TRIGGER AS $$
-	
-	 -- Declare the variables we'll be using.
-	DECLARE
-	  key TEXT;
-	  qry TEXT;
-	  found TEXT;
-	BEGIN
-	
-	  qry := 'SELECT short_url FROM ' || quote_ident(TG_TABLE_NAME) || ' WHERE short_url=';
-	  LOOP
-		key := random_string();
-	
-		EXECUTE qry || quote_literal(key) INTO found;
-		IF found IS NULL THEN
-		  EXIT;
-		END IF;
-	
-	  END LOOP;
-	
-	  
-	  NEW.short_url = key;
-	  RETURN NEW;
-	END;
-	$$ language 'plpgsql';
-	
-	CREATE OR REPLACE TRIGGER trigger_urls_genid BEFORE INSERT ON urls FOR EACH ROW EXECUTE PROCEDURE unique_short_id();
-	`)
+	_, err := db.ExecContext(ctx, "create table if not exists urls(short_url text primary key, original_url text unique);")
 	if err != nil {
 		return err
 	}
